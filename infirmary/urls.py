ddbd9bcc65d301db73bdea1741e78c64e60b791b
Coding Cat <32220838+qtulhu@users.noreply.github.com>
2020-05-23 19:50:43 +0800

    починил страницы

--- infirmary/urls.py
+++ infirmary/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add-patient', views.add, name='add'),
     path('pacient', views.pacient, name='pacient'),
-    path('archive', views.pacient, name='archive'),
-    path('help', views.pacient, name='help'),
+    path('archive', views.archive, name='archive'),
+    path('help', views.help, name='help'),
 ]
