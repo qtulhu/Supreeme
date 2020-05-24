e6886c78f7891bce2bbfdeeadf43359be59ebc0d
Aissilen <ivanlobanov97@gmail.com>
2020-05-25 03:48:14 +0800

    поправил модели и шаблоны

--- infirmary/models.py
+++ infirmary/models.py
@@ -1,39 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.TextField('Имя',max_length=100, blank=True)
-    last_name = models.TextField('Фамилия',max_length=100, blank=True)
-    position = models.TextField('Должность',max_length=100, blank=True)
+    first_name = models.CharField('Имя',max_length=100, blank=True)
+    last_name = models.CharField('Фамилия',max_length=100, blank=True)
+    patronymic = models.CharField('Отчество',max_length=100, blank=True)
+    position = models.CharField('Должность',max_length=100, blank=True)
 
     class Meta:
             verbose_name = 'Доктор'
             verbose_name_plural = 'Доктора'
 
     def __str__(self):
-        return 'Доктор {} {}'.format(self.first_name,self.last_name)
+        return '{} {} {}'.format(self.last_name,self.first_name,self.patronymic)
 
     def get_absolute_url(self):
          return reverse('model-detail-view', args=[str(self.id)])
 
 class Pacient(models.Model):
-    first_name = models.TextField('Имя',max_length=100, blank=True)
-    last_name = models.TextField('Фамилия',max_length=100, blank=True)
-    diagnosis = models.ManyToManyField('Diagnosis',max_length=150, blank=True,help_text="Выберите диагноз")
+    first_name = models.CharField('Имя',max_length=100, blank=True)
+    last_name = models.CharField('Фамилия',max_length=100, blank=True)
+    patronymic = models.CharField('Отчество',max_length=100, blank=True)
+    age = models.CharField('Возраст',max_length=100, blank=True)
+    diagnosis = models.ForeignKey('Diagnosis',max_length=150,on_delete=models.SET_NULL,null=True, blank=True,help_text="Выберите диагноз")
     doctor = models.ForeignKey('Doctor', on_delete=models.SET_NULL, null=True,blank=True)
     procedures = models.ManyToManyField('Procedure',max_length=150, blank=True,help_text="Выберите процедуры")
-    pulse_link = models.TextField('Пульс',max_length=150, blank=True)
-    spo2_link = models.TextField('Содержание кислорода',max_length=150, blank=True)
-    pi_link = models.TextField('Перфузионный Индекс',max_length=150, blank=True)
-    temperature_link = models.TextField('Температура',max_length=150, blank=True)
+    pulse_link = models.CharField('Пульс',max_length=150, blank=True)
+    spo2_link = models.CharField('Содержание кислорода',max_length=150, blank=True)
+    pi_link = models.CharField('Перфузионный Индекс',max_length=150, blank=True)
+    temperature_link = models.CharField('Температура',max_length=150, blank=True)
 
     class Meta:
             verbose_name = 'Пациент'
             verbose_name_plural = 'Пациенты'
 
     def __str__(self):
-        return 'Пациент {} {}'.format(self.first_name,self.last_name)
+        return '{} {} {}'.format(self.last_name,self.first_name,self.patronymic)
 
     def get_absolute_url(self):
          return reverse('model-detail-view', args=[str(self.id)])
--- infirmary/views.py
+++ infirmary/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-
+from .models  import Doctor,Pacient,Diagnosis,Procedure
 
 def index(request):
+
+
+    pacients = Pacient.objects.all()
+
+    all_pacient = []
+    for index in pacients:
+        pacient = {
+            'ill': index,
+            'first_name':index.first_name,
+            'last_name':index.last_name,
+            'age':index.age,
+            'doctor':index.doctor,
+            'diagnosis':index.diagnosis,
+            'procedures':index.procedures,
+            'pulse_link':index.pulse_link,
+            'spo2_link':index.spo2_link,
+            'pi_link':index.pi_link,
+            'temperature_link':index.temperature_link,
+            # 'link':index.get_absolute_url,
+        }
+        all_pacient.append(pacient)
+
+
     return render(
         request,
         'index.html',
+        {'pacient_info':all_pacient},
     )
 
 
